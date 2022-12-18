990a65c0e2dfa09c749576f5cfe7888b2164bd0c
kevinbreaker <kevinbreaker2604@gmail.com>
2022-12-18 12:18:36 -0300

    fix: :bug: fix composables absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@composables': fileURLToPath(new URL('./src/composable', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
     },
   },
