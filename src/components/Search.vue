b05d17d160c39771e7dfe0494ebbebb11c504677
kevinbreaker <kevinbreaker2604@gmail.com>
2022-12-18 22:14:56 -0300

    feat: :sparkles: create Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import Button from '@components/Button.vue'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  placeholder: string
+  modelValue: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', v: string): void
+  (e: 'search'): void
+}>()
+
+const model = computed({
+  get: () => props.modelValue,
+  set: value => emit('update:modelValue', value),
+})
+</script>
+
+<template>
+  <form class="search-form" @submit.prevent="emit('search')">
+    <input
+      v-bind="$attrs"
+      v-model="model"
+      type="search"
+      class="search-input"
+      :placeholder="placeholder"
+    >
+
+    <Button class="button">
+      Enviar
+    </Button>
+  </form>
+</template>
+
+<style lang="scss">
+.search-form {
+  @apply flex items-center;
+  @apply w-full;
+  max-width: 600px;
+
+  > .search-input {
+    @apply flex-grow;
+    @apply h-8 w-full;
+    @apply mr-4 px-4;
+    @apply rounded-md border-none;
+  }
+}
+</style>
