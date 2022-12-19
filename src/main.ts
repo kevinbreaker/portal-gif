3b16243b66d2724730561a0f551e43c4c82e40cc
kevinbreaker <kevinbreaker2604@gmail.com>
2022-12-18 22:14:15 -0300

    feat: :sparkles: configure routers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+// import Router from 'vue-router'
 import App from './App.vue'
+
+import Routers from './router/index'
 
 import './assets/main.scss'
 
 const app = createApp(App)
 
-app.mount('#app')
+app.use(Routers).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/index.vue'),
+    },
+    {
+      path: '/my-portal',
+      name: 'MyPortal',
+      meta: {
+        auth: true,
+      },
+      component: () => import('@/views/MyPortal.vue'),
+    },
+
+    {
+      path: '/:pathMatch(.*)',
+      redirect: () => ({ name: 'Home' }),
+    },
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !window.solana?.publicKey)
+    next({ name: 'Home' })
+  else if (to.name === 'Home' && window.solana?.publicKey?.length)
+    next({ name: 'MyPortal' })
+  else
+    next()
 })
 
 export default router
