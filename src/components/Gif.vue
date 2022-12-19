2fea02bbe909fad9595183777aeb4882e31004b5
kevinbreaker <kevinbreaker2604@gmail.com>
2022-12-18 22:14:38 -0300

    feat: :sparkles: create Gif components

---
+++ src/components/Gif.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineProps<{
+  src: string
+  alt?: string
+}>()
+</script>
+
+<template>
+  <figure class="gif">
+    <img :src="src" :alt="alt" class="image">
+  </figure>
+</template>
+
+<style lang="scss">
+.gif {
+  @apply grid;
+  @apply m-0 mb-4;
+
+  break-inside: avoid;
+
+  > .image {
+    @apply w-full;
+
+    grid-row: 1 / -1;
+    grid-column: 1;
+  }
+}
+</style>
---
+++ src/components/GifsContainer.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import Gif from './Gif.vue'
+
+defineProps<{
+  items: string[]
+}>()
+</script>
+
+<template>
+  <section class="gif-container">
+    <Gif v-for="(image, index) in items" :key="index" :src="image" />
+  </section>
+</template>
+
+<style lang="scss">
+  .gif-container {
+    column-gap: 16px;
+
+    width: 90vw;
+    max-width: 1000px;
+
+    @screen sm {
+      column-count: 1;
+    }
+    @screen md {
+      column-count: 2;
+    }
+    @screen lg {
+      column-count: 4;
+    }
+  }
+  </style>
